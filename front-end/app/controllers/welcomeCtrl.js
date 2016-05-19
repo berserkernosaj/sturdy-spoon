4bf48040a307be5864d239c7082ab9cd3c6ee2f0
berserkernosaj <nosajmahdlo@gmail.com>
2016-05-18 18:56:24 -0600

    still some work left but pushing for safe keeping

--- front-end/app/controllers/welcomeCtrl.js
+++ front-end/app/controllers/welcomeCtrl.js
@@ -1,8 +1,10 @@
-angular.module('harold').controller('welcomeCtrl', function($scope, srvc){
+angular.module('harold').controller('welcomeCtrl', function($scope, $location, srvc){
 
 $scope.test = "Your a boss";
 
-
+$scope.goToStory = function() {
+  $location.path("/story");
+}
 
 
 
